a21f2dbfda9120cf4fb7bfcd430994810b8d7ea4
wszjk2021 <wszjk2021@163.com>
2022-01-29 16:24:44 +0800

    12345

--- static/js/api.js
+++ static/js/api.js
@@ -1,1 +1,1 @@
-(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=3.0&ak=tYLSdsr5kI9c9tjf1Q0CxEPPB8wNgMdG&services=&t=20220111140745"></script>');})();
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=3.0&ak=tYLSdsr5kI9c9tjf1Q0CxEPPB8wNgMdG&services=&t=20220113143055"></script>');})();
